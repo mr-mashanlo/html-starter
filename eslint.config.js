a2a26f586b467a2d8d75eba85c71b4a7e2d6487b
mr-mashanlo <mr.mashanlo@yandex.ru>
2024-08-15 12:02:03 +0500

    create starter

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      indent: [
+        'error',
+        2
+      ],
+      'linebreak-style': [
+        'error',
+        'unix'
+      ],
+      quotes: [
+        'error',
+        'single'
+      ],
+      semi: [
+        'error',
+        'always'
+      ],
+      'object-curly-spacing': [
+        'error',
+        'always'
+      ],
+      'array-bracket-spacing': [
+        'error',
+        'always'
+      ],
+      'space-in-parens': [
+        'error',
+        'always'
+      ],
+      'max-len': [
+        'error',
+        { 'code': 400 }
+      ],
+      'comma-dangle': [
+        'error',
+        { arrays: 'never', objects: 'never', imports: 'never', exports: 'never', functions: 'never' }
+      ]
+    }
+  },
+  pluginJs.configs.recommended
+];
